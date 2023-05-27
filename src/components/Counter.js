9d8de4954dc683245dc8201d46a235b34951625a
Ayush Gupta <118269589+AyushGupta-0@users.noreply.github.com>
2023-05-27 19:20:17 +0530

    managing new slice

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,34 +1,34 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch  } from "react-redux";
 // import { Component } from "react";
+import { counterActions } from "../store/index";
 
 const Counter = () => {
   const dispatch = useDispatch();
 
-  const counter = useSelector((state) => state.counter);
+  const counter =useSelector((state)=>state.counter);
+  const show =useSelector((state)=>state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: "increase" , amount : 5 });
+    dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = (state) => {
-    return {
-      counter: state.counter + 1,
-    };
+    dispatch(counterActions.toggleCounter())
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
